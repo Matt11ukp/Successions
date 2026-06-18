4a4eae9b711c3c826c6dc14f03faf2c664fe433d
Freddy Ordonez <freddy2007alexander.@gmail.com>
2026-06-18 11:08:05 -0500

    Autoria

--- src/main/java/edu/unl/cc/succession/business/PrimeNumberOddUpToTerm.java
+++ src/main/java/edu/unl/cc/succession/business/PrimeNumberOddUpToTerm.java
@@ -1,4 +1,10 @@
 package edu.unl.cc.succession.business;
+/**
+ * @author Freddy
+ * Esta clase calcula la suma de los primeros n términos de la sucesión de números
+ * primos elevados a potencias impares.
+ *
+ */
 
 public class PrimeNumberOddUpToTerm extends PrimeNumber{
     public PrimeNumberOddUpToTerm(Integer limit){
@@ -8,6 +14,7 @@ public class PrimeNumberOddUpToTerm extends PrimeNumber{
     public PrimeNumberOddUpToTerm(Integer start, Integer limit){
         super(start, limit);
     }
+
 
     @Override
     public Number calculate() {
