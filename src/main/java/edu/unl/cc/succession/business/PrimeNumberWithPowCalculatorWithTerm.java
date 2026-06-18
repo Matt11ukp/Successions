5d87665e1b30127fc3b178b5afcb9cc402060fff
Aria Quezada <ariaquezafer@gmail.com>
2026-06-18 15:28:14 -0500

    API

--- src/main/java/edu/unl/cc/succession/business/PrimeNumberWithPowCalculatorWithTerm.java
+++ src/main/java/edu/unl/cc/succession/business/PrimeNumberWithPowCalculatorWithTerm.java
@@ -1,17 +1,20 @@
 package edu.unl.cc.succession.business;
+
+import edu.unl.cc.succession.model.Printable;
+import edu.unl.cc.succession.model.Succesionable;
 /**
- * Representa la serie de numeros primos elevados a la raiz cubica con N terminos
- *  (S = 1^(1/3) + 3^(1/3) + 5^(1/3) + 7^(1/3) + 11^(1/3) + 13^(1/3)):
+ * Representa la serie de numeros primos elevados a la raiz de impares con N terminos
+ * (S = 1^(1/1) + 3^(1/3) + 5^(1/5) + 7^(1/7) + 11^(1/9) + 13^(1/11)):
  * @author Matias Romero, Freddy Ordoñez, Luis Armijos, Ezequiel Chamba, Arlette Quezada
  */
 
-public class PrimeNumberWithPowCubicCalculatorWithTerm extends PrimeNumber{
+public class PrimeNumberWithPowCalculatorWithTerm extends PrimeNumber {
 
-    public PrimeNumberWithPowCubicCalculatorWithTerm(Integer limit) {
+    public PrimeNumberWithPowCalculatorWithTerm(Integer limit) {
         super(limit);
     }
 
-    public PrimeNumberWithPowCubicCalculatorWithTerm(Integer start, Integer limit) {
+    public PrimeNumberWithPowCalculatorWithTerm(Integer start, Integer limit) {
         super(start, limit);
     }
 
@@ -20,15 +23,17 @@ public class PrimeNumberWithPowCubicCalculatorWithTerm extends PrimeNumber{
         double result = 0;
         int countTerm = 0;
         final int numeratorExponent = 1;
-        int denominatorExponent = 3;
+        int denominatorExponent = 1;
         while(countTerm < limit){
             this.printableTerms.append(currentTerm).append("^(")
                     .append(numeratorExponent).append("/").append(denominatorExponent).append(") +");
             result = result + Math.pow(currentTerm, (double) numeratorExponent / denominatorExponent);
 
             currentTerm = nextTerm(currentTerm).intValue();
+            denominatorExponent += 2;
             countTerm++;
         }
         return result;
     }
+
 }
